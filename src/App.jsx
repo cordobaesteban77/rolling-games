78fa84c067c1c4f093a960d0bd91acbd37c9746b
Córdoba Esteban Augusto <cordobaesteban77@gmail.com>
2025-04-28 20:23:15 -0300

    dejo listo el app para el push

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import React, { useEffect } from 'react'
-import {getGames} from "./data/api.js"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginScreen from './pages/LoginScreen.jsx'
 
 const App = () => {
-  useEffect(() => {
-    getGames().then((respuesta) => {
-      console.log(respuesta)
-    })
-  }, [])
-  
-  
-  
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginScreen />} />
-        </Routes>
-      </BrowserRouter>
+      
     </div>
   )
 }
-
 export default App
